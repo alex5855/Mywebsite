9ba16ddf0d168f5625303ecdc0a0daaa1a94e45c
Habilla <demoalex58@gmail.com>
2022-01-08 11:34:09 -0500

    Add gitignore , procfile , and port

--- app.js
+++ app.js
@@ -80,11 +80,15 @@ app.post("/mycontact",(req,res)=>{
 
 
 
+let port = process.env.PORT;
+if(port == null || port==""){
+    port= 3000;
+}
+app.listen(port);
 
 
 
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server started on port`);
 });
